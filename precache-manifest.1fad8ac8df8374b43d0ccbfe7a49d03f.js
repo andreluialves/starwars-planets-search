8c238a656643bda194dc3c207f1e585052e2c67f
André Alves <andreluialves@hotmail.com>
2022-08-11 20:44:04 -0300

    Updates

---
+++ precache-manifest.1fad8ac8df8374b43d0ccbfe7a49d03f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e9ee11e308160e22da7b50bd1dbdf0",
+    "url": "/starwars-planets-search/index.html"
+  },
+  {
+    "revision": "3b1f5f89dd97109554fa",
+    "url": "/starwars-planets-search/static/css/2.171a7357.chunk.css"
+  },
+  {
+    "revision": "5d34d1711fe147c337bb",
+    "url": "/starwars-planets-search/static/css/main.8294207d.chunk.css"
+  },
+  {
+    "revision": "3b1f5f89dd97109554fa",
+    "url": "/starwars-planets-search/static/js/2.f8d48c36.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-planets-search/static/js/2.f8d48c36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d34d1711fe147c337bb",
+    "url": "/starwars-planets-search/static/js/main.7d35f688.chunk.js"
+  },
+  {
+    "revision": "1b9e9e3eb9707d3157b0",
+    "url": "/starwars-planets-search/static/js/runtime-main.d5e8347d.js"
+  }
+]);
